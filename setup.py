7be08b56d7168c3845cdcc9a8be08190d65b82e9
Martin <martin.preusse@gmail.com>
2020-08-18 19:17:40 +0200

    increase version to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from os import path
 
 setup(name='datacroaker',
-      version='0.0.2',
+      version='0.0.3',
       description='Framework for managing data from defined data sources.',
       url='https://github.com/kaiserpreusse/datacroaker',
       author='Martin Preusse',
